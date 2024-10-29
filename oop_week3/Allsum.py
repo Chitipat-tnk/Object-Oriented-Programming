005527d6bae70327d5961e9a53f26e5f2755c0fb
Chitipat-tnk <67319010058@tnk.ac.th>
2024-10-29 17:58:52 +0700

    Allsum

---
+++ oop_week3/Allsum.py
@@ -0,0 +1,10 @@
+import random
+print('โปรเเกรมเป่ายิงฉุบ')
+while True:
+    a = random.choice(["ค้อน","กรรไกร","กระดาษ"])
+    print(f'{a}')
+    print("ค้อน ","กรรไกร ","กระดาษ ")
+    Value = str(input('คุณเลือก : '))
+    if Value != a:
+        print("ผลลัพท์คือ เเพ้")
+        print("------------------ ")
